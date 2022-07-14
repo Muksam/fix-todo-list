9f795904504fe7fde129439124c7a951d032e921
Muksam <mukdam@gmail.com>
2022-07-14 12:17:13 +0545

    final

---
+++ src/components/todo-form/todo-form.jsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { nanoid } from 'nanoid';
+import { TodosContext } from '../../todo-context';
+import './todo-form.scss';
+
+export const TodoForm = () => {
+  const { todos, setTodos } = React.useContext(TodosContext);
+
+  const [task, setTask] = React.useState('');
+
+  const handleAddTodo = () => {
+    // Fin an ability to add new task
+    if (task) {
+      setTodos((prev) => [...prev, {
+        id: nanoid(),
+        label: task,
+        checked: false,
+      }]);
+    }
+    setTask('');
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      handleAddTodo();
+    }
+  };
+
+  return (
+    <div className="todo-form">
+      <input
+        placeholder="Enter new task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        onKeyUp={handleKeyUp}
+      />
+      <button type="button" onClick={handleAddTodo}>
+        Add task
+      </button>
+    </div>
+  );
+};
